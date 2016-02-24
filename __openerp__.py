bf24bcb631cb313567f8787f091f8783c958d684
Valentin Thirion <valentinthirion@gmail.com>
2016-02-24 16:51:55 +0100

    First version

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Stock Delivery Note",
+    'version': '1.0',
+    'depends': ['stock',],
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Warehouse',
+    'description': """
+Stock delivery report
+
+This modules creates a report for deliveries that will be exported and printed for the customer.
+
+This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
+""",
+    'data': [
+            'report/stock_delivery_report.xml',
+             ],
+}
